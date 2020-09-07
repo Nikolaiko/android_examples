f051426a26102875793a6d1418b75af5ff9ebce4
Nikolay Baklanov <>
2020-09-07 18:36:40 +0600

    OpenCV example.

---
+++ MyApplication/app/src/main/java/com/app/myapplication/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.app.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val (q,e) = fff()
+
+        println("$q     $e")
+    }
+}
---
+++ MyApplication/app/src/main/java/com/app/myapplication/fff.kt
@@ -0,0 +1,7 @@
+package com.app.myapplication
+
+data class fff(
+    val r:Int = 6,
+    val h:Int = 7,
+    val j:Int = 8
+)
