97b8ac2723e207913c1df98be5349519fe9bd89c
Nikolai Baklanov <nikolaiko@mail.ru>
2022-10-18 09:36:18 +0600

    Tasks

--- CodeSignal/Arcade/Intro/ArcadeTasks/src/main/kotlin/groups/ExploringTheWaters.kt
+++ CodeSignal/Arcade/Intro/ArcadeTasks/src/main/kotlin/groups/ExploringTheWaters.kt
@@ -21,4 +21,54 @@ class ExploringTheWaters {
         return result
     }
 
+    fun areSimilars(a: MutableList<Int>, b: MutableList<Int>): Boolean {
+        var differences = 0
+        val firstOut = mutableListOf<Int>()
+        val secondOut = mutableListOf<Int>()
+        for (index in a.indices) {
+            if (a[index] != b[index]) {
+                differences += 1
+                firstOut.add(a[index])
+                secondOut.add(b[index])
+                if (differences > 2) {
+                    return false
+                }
+            }
+        }
+        return firstOut.containsAll(secondOut)
+    }
+
+    fun arrayChange(inputArray: MutableList<Int>): Int {
+        var totalDifference = 0
+        for (index in 0..inputArray.size - 2) {
+            if (inputArray[index] >= inputArray[index + 1]) {
+                val localDifference = inputArray[index] - inputArray[index + 1] + 1
+                inputArray[index + 1] += localDifference
+
+                totalDifference += localDifference
+            }
+        }
+        return totalDifference
+    }
+
+    fun palindromeRearranging(input: String): Boolean {
+        val isLengthEven = input.length % 2 == 0
+        val lettersMap = mutableMapOf<Char, Int>()
+
+        input.forEach { lettersMap[it] = (lettersMap[it] ?: 0) + 1 }
+
+        var oddLettersCount = 0
+        lettersMap.forEach {
+            if (it.value % 2 != 0) {
+                oddLettersCount += 1
+            }
+
+            when(isLengthEven) {
+                true -> { if (oddLettersCount > 0) return false }
+                false -> { if (oddLettersCount > 1) return false }
+            }
+        }
+        return true
+    }
+
 }
--- CodeSignal/Arcade/Intro/ArcadeTasks/src/test/kotlin/arcadeTests/exploringTheWaters/ExploringTheWatersTests.kt
+++ CodeSignal/Arcade/Intro/ArcadeTasks/src/test/kotlin/arcadeTests/exploringTheWaters/ExploringTheWatersTests.kt
@@ -4,6 +4,8 @@ import groups.ExploringTheWaters
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ExploringTheWatersTests {
@@ -27,5 +29,33 @@ class ExploringTheWatersTests {
                 "ded"
             )
         ))
+    }
+
+    @Test
+    fun testAreSimilar() {
+        assertEquals(true, exploring.areSimilars(mutableListOf(1, 2, 3), mutableListOf(2, 1, 3)))
+        assertEquals(false, exploring.areSimilars(mutableListOf(1, 2, 2), mutableListOf(2, 1, 1)))
+        assertEquals(
+            true,
+            exploring.areSimilars(mutableListOf(1, 2, 1, 2, 2, 1), mutableListOf(2, 2, 1, 1, 2, 1))
+        )
+        assertEquals(
+            false,
+            exploring.areSimilars(mutableListOf(1, 1, 4), mutableListOf(1, 2, 3))
+        )
+    }
+
+    @Test
+    fun arrayChangeTest() {
+        assertEquals(12, exploring.arrayChange(mutableListOf(2, 1, 10, 1)))
+        assertEquals(3, exploring.arrayChange(mutableListOf(1, 1, 1)))
+        assertEquals(13, exploring.arrayChange(mutableListOf(2, 3, 3, 5, 5, 5, 4, 12, 12, 10, 15)))
+    }
+
+    @Test
+    fun testPalindromeRearranging() {
+        assertTrue(exploring.palindromeRearranging("aabb"))
+        assertTrue(exploring.palindromeRearranging("zaa"))
+        assertFalse(exploring.palindromeRearranging("abcad"))
     }
 }
