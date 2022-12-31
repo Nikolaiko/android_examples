7b8a8b1a9dc129e40a98cef9239cad0259bd7f01
Nikolai Baklanov <nikolaiko@mail.ru>
2023-01-01 00:56:34 +0600

    Two tasks.

--- CodeSignal/Arcade/Intro/ArcadeTasks/src/main/kotlin/groups/intro/ThroughTheFog.kt
+++ CodeSignal/Arcade/Intro/ArcadeTasks/src/main/kotlin/groups/intro/ThroughTheFog.kt
@@ -1,5 +1,30 @@
 package groups.intro
 
-class ThroughTheFog {
+import kotlin.math.ceil
+import kotlin.math.log
 
+class ThroughTheFog {
+    fun circleOfNumbers(n: Int, firstNumber: Int): Int {
+        val halfValue = (n / 2) - 1
+        val maxValue = n - 1
+
+        return when {
+            firstNumber == halfValue -> maxValue
+            firstNumber == maxValue -> halfValue
+            firstNumber < halfValue -> firstNumber + (n / 2)
+            else -> firstNumber - (n / 2)
+        }
+    }
+
+    fun depositProfit(deposit: Int, rate: Int, threshold: Int): Int {
+//        var currentDeposit = deposit
+//        var yearsCount = 0
+//        do {
+//            currentDeposit += (currentDeposit * rate) / 100
+//            yearsCount += 1
+//        } while (currentDeposit < threshold)
+        val percents: Double = rate.toDouble() / 100
+        val differense: Double = threshold.toDouble() / deposit.toDouble()
+        return ceil(log(differense, (1 + percents))).toInt()
+    }
 }
--- CodeSignal/Arcade/Intro/ArcadeTasks/src/test/kotlin/arcadeTests/intro/throughTheFog/ThroughTheFogTests.kt
+++ CodeSignal/Arcade/Intro/ArcadeTasks/src/test/kotlin/arcadeTests/intro/throughTheFog/ThroughTheFogTests.kt
@@ -1,5 +1,25 @@
 package arcadeTests.intro.throughTheFog
 
-class ThroughTheFogTests {
+import groups.intro.SmoothSailing
+import groups.intro.ThroughTheFog
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class ThroughTheFogTests {
+    private val fog = ThroughTheFog()
+
+    @Test
+    fun circleOfNumbersTests() {
+        assertEquals(7, fog.circleOfNumbers(10, 2))
+        assertEquals(3, fog.circleOfNumbers(4, 1))
+    }
+
+    @Test
+    fun depositProfitTests() {
+        assertEquals(3, fog.depositProfit(100, 20, 170))
+        assertEquals(6, fog.depositProfit(1, 100, 64))
+    }
 }
